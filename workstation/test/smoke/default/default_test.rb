a67a382b6dbadaf47c3dd846a8c3d6e05d388726
Ryczor <ryczor@gmail.com>
2019-07-20 21:17:30 +0200

    Using templates for MYIIS on windows 2012 server

---
+++ workstation/test/smoke/default/default_test.rb
@@ -0,0 +1,27 @@
+# # encoding: utf-8
+
+# Inspec test for recipe workstation::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+#unless os.windows?
+  # This is an example test, replace with your own test.
+  # describe user('root'), :skip do
+  #  it { should exist }
+  # end
+#end
+
+# This is an example test, replace it with your own test.
+#describe port(80), :skip do
+#  it { should_not be_listening }
+#end
+
+system_policies = 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System'
+
+describe registry_key('System Policies', system_policies) do 
+  its('EnableLUA') { should eq 0 }
+  its('ConsentPromptBehaviorAdmin') { should eq 0 }
+end
+
+
